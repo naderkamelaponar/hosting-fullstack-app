c2966bd67431d68ac541d92d8c2eeb7cda151ae4
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-04-24 21:33:09 +0200

    puppeteer

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -12,7 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),process.env.CHROME_BIN
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
